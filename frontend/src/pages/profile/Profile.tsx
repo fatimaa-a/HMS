9a1979907845aa7dd95927a8d8212d1c33e454b6
dua <duasheixh@gmail.com>
2026-09-10 14:10:49 +0500

    Fix profile API URL

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -11,7 +11,7 @@ import {
 
 import type { User } from "../../types/user";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://hms-1-eav6.onrender.com";
 
 function Profile() {
   const { token } = useAuth();
