635b6d98cfde399cf05825bb7ab128d4578fdc58
dua <duasheixh@gmail.com>
2026-09-14 15:45:43 +0500

    Add database session dependency

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, echo= True)
-SessionLocal = sessionmaker(bind=engine, autoflush= False, autocommit = False)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
